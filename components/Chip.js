4dafbf8a21e0167cfed0553f7643ad81da1ad5e5
Mike Qin <1mike12@gmail.com>
2017-09-24 21:28:34 -0400

    make chips not suck

--- components/Chip.js
+++ components/Chip.js
@@ -1,46 +1,65 @@
 import React from 'react';
-import {Button, FlatList, Text, View} from "react-native";
+import {
+    Button, FlatList, Text, View, StyleSheet, TouchableNativeFeedback, TouchableHighlight,
+    TouchableOpacity
+} from "react-native";
 import http from '../services/http';
 import styles from "../styles";
 
-const chipContainer = {
-    borderRadius: 80,
-    paddingLeft: 12,
-    paddingRight: 12,
-    height: 32,
-    backgroundColor: "#DDD",
-};
+const chipStyles = StyleSheet.create({
+    chipContainer: {
+        borderRadius: 80,
+        paddingLeft: 12,
+        paddingRight: 6,
+        height: 32,
+        backgroundColor: "#DDD",
+        flexDirection: "row",
+        alignItems: "center"
 
-const chipText = {
-    fontSize: 16,
-};
-
-const closeButton = {
-    marginLeft: 4,
-    height: 16,
-    width: 16,
-    borderRadius: 80,
-    backgroundColor: "#333",
-    color: "#FFF",
-    textAlign: "center",
-    textAlignVertical: "center",
-    fontSize: 8
-};
+    },
+    chipText: {
+        fontSize: 16,
+    },
+    closeButton: {
+        marginLeft: 4,
+        height: 24,
+        width: 24,
+        borderRadius: 80,
+        backgroundColor: "#777",
+        color: "#FFF",
+        textAlign: "center",
+        textAlignVertical: "center",
+        fontSize: 12
+    }
+});
 
 export default class Chip extends React.Component {
 
+    /**
+     * @param props onDelete callback
+     */
     constructor(props){
         super(props);
         this.state = {
             text: props.text
         };
+        this.deletePress = this.deletePress.bind(this);
+    }
+
+    deletePress(){
+        if (this.props.onDelete){
+            this.props.onDelete();
+        }
     }
 
     render(){
         return (
-            <View style={chipContainer}>
-                <Text style={chipText}>{this.state.text}</Text>
-                <Text style={closeButton}>X</Text>
+            <View style={chipStyles.chipContainer}>
+                <Text style={chipStyles.chipText}>{this.state.text}</Text>
+                <TouchableOpacity onPress={this.deletePress}
+                                         background={TouchableNativeFeedback.SelectableBackground()}>
+                    <Text style={chipStyles.closeButton}>X</Text>
+                </TouchableOpacity>
             </View>
         );
     }
