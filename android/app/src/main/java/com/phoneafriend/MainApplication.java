225dd4112238c56a3a89567dc6ce338162336787
1mike12 <1mike12@gmail.com>
2017-10-02 11:29:31 -0400

    add icons

--- android/app/src/main/java/com/phoneafriend/MainApplication.java
+++ android/app/src/main/java/com/phoneafriend/MainApplication.java
@@ -3,6 +3,7 @@ package com.phoneafriend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends NavigationApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new ReactNativeConfigPackage(),
             new WebRTCModulePackage()
       );
