6945d84be754b054b394451531c4709a4b6b63ce
1mike12 <1mike12@gmail.com>
2017-10-04 14:58:35 -0400

    add fabs to callscreen

--- screens/CallScreen.js
+++ screens/CallScreen.js
@@ -24,6 +24,11 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const ta = timeAgo();
 const NAME = "CallScreen";
+
+const CAMERA_STATE_REAR = "rear";
+const CAMERA_STATE_FRONT = "front";
+const CAMERA_STATE_OFF = "off";
+
 export default class CallScreen extends React.Component {
 
     constructor(props){
@@ -31,13 +36,43 @@ export default class CallScreen extends React.Component {
         this.state = {
             session: this.props.session,
             ready: true,
+            cameraState: CAMERA_STATE_FRONT
         };
 
         setTimeout(() => this.setState({ready: true}), 3000)
+
+        this.getCameraFab = this.getCameraFab.bind(this);
     }
 
     static getName(){
         return `${config.name}.${NAME}`
+    }
+
+    getCameraFab(){
+        console.log("cameraState", this.state.cameraState)
+        if (this.state.cameraState === CAMERA_STATE_REAR) {
+            return <Fab style={{backgroundColor: "#AAA", margin: 12}}
+                        onPress={() => this.setState({cameraState: CAMERA_STATE_FRONT})}
+                        inside={<Icon name="camera-front-variant"
+                                      size={32}
+                                      color="white"/>
+                        }
+            />
+        } else if (this.state.cameraState === CAMERA_STATE_FRONT) {
+            return <Fab style={{backgroundColor: "#AAA", margin: 12}}
+                        onPress={() => this.setState({cameraState: CAMERA_STATE_REAR})}
+                        inside={<Icon name="camera-rear-variant"
+                                      size={32}
+                                      color="white"/>
+                        }
+            />
+        } else {
+            return <Fab style={{backgroundColor: "#AAA", margin: 12}}
+                        inside={<Icon name="test-tube"
+                                      size={32}
+                                      color="white"/>}
+            />
+        }
     }
 
     render(){
@@ -53,26 +88,21 @@ export default class CallScreen extends React.Component {
                     </View> :
                     <View style={{position: "absolute", top: 0, bottom: 0, left: 0, right: 0, backgroundColor: "#EEE"}}>
                         <Text>Loaded</Text>
-                        <View style={{position: "absolute", bottom: 0, flexDirection: 'row'}}>
+
+                        <View style={{
+                            position: "absolute",
+                            bottom: 0,
+                            flexDirection: 'row',
+                            justifyContent: "space-between"
+                        }}>
                             <Fab style={{backgroundColor: "#AAA", margin: 12}}
                                  inside={<Icon name="camera"
                                                size={32}
                                                color="white"/>
                                  }
                             />
-                            <Fab style={{backgroundColor: "#AAA", margin: 12}}
-                                 inside={<Icon name="camera-front-variant"
-                                               size={32}
-                                               color="white"/>
-                                 }
-                            />
-                            <Fab style={{backgroundColor: "#AAA", margin: 12}}
-                                 inside={<Icon name="camera-rear-variant"
-                                               size={32}
-                                               color="white"/>
-                                 }
-                            />
-                            <Fab style={{backgroundColor: "#AAA", margin: 12}}
+                            {this.getCameraFab()}
+                            <Fab style={{backgroundColor: "green", margin: 12}}
                                  inside={<Icon name="message-text"
                                                size={32}
                                                color="white"/>
