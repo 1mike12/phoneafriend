c2c4d3ca968b9ffc34298d0aa0be6490074eb714
Mike Qin <1mike12@gmail.com>
2017-10-13 18:43:24 -0400

    fix authenticated

--- server/router/middleware/authenticated.js
+++ server/router/middleware/authenticated.js
@@ -1,9 +1,6 @@
-const jwt = require("jsonwebtoken");
-const config = require("../../config");
 const _ = require("lodash");
 const AuthenticationService = require("../../services/AuthenticationService");
-const whitelistKeys = new Set();
-whitelistKeys.add("iat");
+const WHITELISTED_HEADER_KEYS = new Set(["iat"]);
 
 module.exports = function(req, res, next){
     const path = req.originalUrl;
@@ -17,15 +14,18 @@ module.exports = function(req, res, next){
         AuthenticationService.authenticate(token)
         .then((payload) =>{
             //extend all token payload directly onto req object, unless it's collision
+            // req.userId = payload.userId;
             _.forEach(payload, (value, key) =>{
-                if (req.key === undefined && !whitelistKeys.has(key)){
+                if (req.key === undefined && !WHITELISTED_HEADER_KEYS.has(key)){
                     req[key] = value
                 } else {
-                    throw new Error(`collsion with extending jwt token key and preexisting req key: ${key}`)
+                    throw new Error(`collision with extending jwt token key and preexisting req key: ${key}`)
                 }
             });
-            // req.userId = payload.userId;
-            return next();
+
+            next();
+            //to disable warning about runaway bluebird promises?? not really sure why this happens to begin with
+            return null;
         })
         .catch((err) =>{
             return res.status(400)
