2ed2abe16fa68860003d2f5249d2a521754642d1
Mike Qin <1mike12@gmail.com>
2017-10-21 18:21:07 -0400

    fix path for static files

--- server/router/server.js
+++ server/router/server.js
@@ -6,11 +6,9 @@ const config = require("../config");
 const os = require("os");
 const http = require("http");
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(__dirname + '/public_dist'));
-} else {
-    app.use(express.static(__dirname + '/public'));
-}
+let publicPath = process.env.NODE_ENV === "production" ? '/public_dist' : '/public';
+const fullPath = path.join(process.cwd(), publicPath);
+app.use(express.static(fullPath));
 
 app.use(["/api/*"], BodyParser.json());
 app.use(["/api/*"], BodyParser.urlencoded({extended: true}));
@@ -28,19 +26,19 @@ app.use("/api/public/", require("./public/api_public"));
 let server = http.Server(app);
 
 const PORT = process.env.NODE_ENV === "test" ? config.testPort : config.port;
-server.listen(PORT, function () {
+server.listen(PORT, function(){
     const port = server.address().port;
 
     console.log(`Started on ${getIpAddress()}:${port}`);
     console.log(`Environment: ${process.env.NODE_ENV}`);
 });
 
-function getIpAddress() {
+function getIpAddress(){
     let ifaces = os.networkInterfaces();
     let address = "";
-    Object.keys(ifaces).forEach((ifname) => {
-        ifaces[ifname].forEach((iface) => {
-            if ('IPv4' !== iface.family || iface.internal !== false) {
+    Object.keys(ifaces).forEach((ifname) =>{
+        ifaces[ifname].forEach((iface) =>{
+            if ('IPv4' !== iface.family || iface.internal !== false){
                 // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
                 return;
             }
