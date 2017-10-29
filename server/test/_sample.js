ec15ec7efee7bdd78c1df244bb3eee23ed1cdaf9
Mike Qin <1mike12@gmail.com>
2017-10-29 17:25:56 -0400

    sample works

--- server/test/_sample.js
+++ server/test/_sample.js
@@ -1,4 +1,14 @@
 //top level because these have to work before all others
+// let sandbox;
+//
+// beforeEach(() =>{
+//     sandbox = sinon.sandbox.create();
+//     // sandbox.stub(global, 'setTimeout', setImmediate);
+// })
+//
+// afterEach(() =>{
+//     sandbox.restore();
+// })
 describe("_sample", function(){
 
     beforeEach(function(done){
@@ -23,48 +33,71 @@ describe("_sample 2", function(){
 
 describe("async await and fake timers", () =>{
 
-    it(`should delay execution by 1 second`, function(){
-        const clock = sinon.useFakeTimers();
+    async function asyncAwait(){
+        await new Promise(function(resolve){
+            setTimeout(() => resolve(2), 5000);
+        })
 
-        return Promise.resolve()
-        .then(() =>{
-            return new Promise(function(resolve){
-                setTimeout(resolve, 1000); // resolve never gets called
-                clock.tick(1000);
-            });
+        let x = 1;
+        await new Promise(function(resolve){
+            setTimeout(() => resolve(2), 5000);
         })
-        .then(() =>{
-            let x = 0
+        return x;
+    }
+
+    it(`normal promises`, async function(){
+        let clock = sinon.useFakeTimers();
+        let x = 0;
+        let promise = new Promise(function(resolve){
+            setTimeout(() => resolve(2), 5000);
         })
+        .then((x) =>{
+            return expect(x).to.equal(2)
+        })
+
+        clock.tick(1001);
+        clock.restore();
     });
 
-    it("should be able to use timers", done =>{
-        let x = 0;
-        let clock = sinon.useFakeTimers()
-        setTimeout(() =>{
-            x = 1
-        }, 10);
+    it("use external async", () =>{
+        let clock = sinon.useFakeTimers();
 
+        asyncAwait()
+        .then(x =>{
+            expect(x).to.equal(1)
+        })
 
-        expect(x).to.equal(0)
-        clock.tick(1000)
-        expect(x).to.equal(1)
-        clock.restore()
-        done();
+        clock.tick(1001);
+        clock.restore();
     })
-    it("async await and fake timers", async () =>{
-        let clock = sinon.useFakeTimers()
 
-        function getNumber(){
-            return new Promise(function(resolve){
-                setTimeout(resolve(1), 5000); // resolve never gets called
-            });
-        }
-
-        let x = 0;
-        x = await getNumber()
-        clock.tick(5000)
+    it(`takes 2 real seconds`, async function(){
+        let x = await new Promise((resolve) =>{
+            setTimeout(() => resolve(1), 2000);
+        })
         expect(x).to.equal(1)
+    });
+
+    it(`takes 0 seconds`, async function(){
+        let clock = sinon.useFakeTimers();
+        let x = await  new Promise((resolve) =>{
+            setTimeout(() => resolve(1), 2000);
+            clock.tick(2000)
+        })
         clock.restore()
-    })
+        expect(x).to.equal(1)
+    });
+
+    it(`doesnt work Process finished with exit code 0`, async function(){
+        let clock = sinon.useFakeTimers();
+        //does not run past next call
+        let x = await  new Promise((resolve) =>{
+            setTimeout(() =>{
+                resolve(1)
+            }, 2000);
+        })
+        clock.tick(2000)
+        clock.restore()
+        expect(x).to.equal(1)
+    });
 })
