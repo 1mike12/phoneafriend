f0c7d8c9800d5d228e32c21b7897b3b93fd76bcd
Mike Qin <1mike12@gmail.com>
2017-10-29 16:18:06 -0400

    add clock samples

--- server/test/_sample.js
+++ server/test/_sample.js
@@ -20,3 +20,51 @@ describe("_sample 2", function(){
         done();
     });
 });
+
+describe("async await and fake timers", () =>{
+
+    it(`should delay execution by 1 second`, function(){
+        const clock = sinon.useFakeTimers();
+
+        return Promise.resolve()
+        .then(() =>{
+            return new Promise(function(resolve){
+                setTimeout(resolve, 1000); // resolve never gets called
+                clock.tick(1000);
+            });
+        })
+        .then(() =>{
+            let x = 0
+        })
+    });
+
+    it("should be able to use timers", done =>{
+        let x = 0;
+        let clock = sinon.useFakeTimers()
+        setTimeout(() =>{
+            x = 1
+        }, 10);
+
+
+        expect(x).to.equal(0)
+        clock.tick(1000)
+        expect(x).to.equal(1)
+        clock.restore()
+        done();
+    })
+    it("async await and fake timers", async () =>{
+        let clock = sinon.useFakeTimers()
+
+        function getNumber(){
+            return new Promise(function(resolve){
+                setTimeout(resolve(1), 5000); // resolve never gets called
+            });
+        }
+
+        let x = 0;
+        x = await getNumber()
+        clock.tick(5000)
+        expect(x).to.equal(1)
+        clock.restore()
+    })
+})
