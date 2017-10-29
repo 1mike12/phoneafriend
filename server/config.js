0231a07043e028e31a12d5480a2a1c3d0f63e996
Mike Qin <1mike12@gmail.com>
2017-10-28 21:24:42 -0400

    change db name

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
     port: 8009,
     testPort: 9009,
     jwtSecret: "1231352tSDQ@#)6",
-    databaseName: "phone_a_friend",
+    databaseName: "phoneafriend",
     get databaseNameTest(){
         return this.databaseName + "_test"
     }
